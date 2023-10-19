9a4e7da20408056c18fc10c9830b0f39d1d145c9
Luisa Barbosa <128723304+Sofiapol456@users.noreply.github.com>
2023-10-19 11:58:58 -0500

    Update desplie.php
    2

--- desplie.php
+++ desplie.php
@@ -4,3 +4,16 @@ include("funciones.php");
 
 // se invoca la function consulta
 echo area();
+
+// se invoca la function consulta
+echo area();
+
+// se invoca la function consulta
+echo conteo();
+
+// se invoca la function consulta
+echo insertar_personas();
+
+// se invoca la function consulta
+echo borrar_persona();
+

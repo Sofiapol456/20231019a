e064f5f885799f4e86e61059a62ad0918054dc8e
Luisa Barbosa <128723304+Sofiapol456@users.noreply.github.com>
2023-10-19 11:55:17 -0500

    Update funciones.php
    mas

--- funciones.php
+++ funciones.php
@@ -32,3 +32,110 @@ $conexion -> close();
 return $salida;
 
 }
+
+<?php
+
+function area(){
+
+// inicializa la variable
+$salida = 0; 
+
+// se conecta con la base de datos
+$conexion = mysqli_connect('localhost', 'root', 'root', 'calculo');
+
+// se acomoda los numeros 
+$sql = "select 2 + 1";
+
+//hace la suma
+$sql .= " as suma";
+
+// muestra la base
+$r = $conexion -> query($sql);
+
+//recorre el recordset
+while($fila = mysqli_fetch_assoc($r))
+{
+
+// incrementa o aculula
+$salida += $fila['suma'];  
+
+}
+
+$conexion -> close();
+
+//retorna la operacion
+return $salida;
+
+}
+
+
+<?php
+
+function conteo(){
+
+// inicializa la variable
+$salida = 0; 
+
+// se conecta con la base de datos
+$conexion = mysqli_connect('localhost', 'root', 'root', 'db_iniciar_sesion_45');
+
+// se acomoda los numeros 
+$sql = "select count(*) from db_iniciar_sesion_45;";
+
+// ejecuta la consulta
+$r = $conexion -> query($sql);
+
+//recorre el recordset
+while($fila = mysqli_fetch_assoc($r))
+{
+
+// incrementa o aculula
+$salida = $fila ['count(*)'];  
+
+}
+
+//cerrado de la conexion
+mysqli_close($conexion);
+
+//retorna la operacion
+return $salida;
+
+}
+
+
+<?php
+function insertar_personas(){
+// inicializa la variable
+$salida = 0; 
+// se conecta con la base de datos
+$conexion = mysqli_connect('localhost', 'root', 'root', 'db_iniciar_sesion_45');
+// se acomoda los numeros 
+$sql = " insert into db_iniciar_sesion_45 (dni_usuario, nombre_de_usuario)";
+$sql .= " values ('3', 'clara')";
+// ejecuta la consulta
+$r = $conexion -> query($sql);
+//recorre el recordset
+$salida=mysqli_affected_rows($conexion);
+//cerrado de la conexion
+mysqli_close($conexion);
+//retorna la operacion
+return $salida;
+
+
+<?php
+function borrar_personas($id){
+// inicializa la variable
+$salida = 0; 
+// se conecta con la base de datos
+$conexion = mysqli_connect('localhost', 'root', 'root', 'db_iniciar_sesion_45');
+// se acomoda los numeros 
+$sql = "delete from db_iniciar_sesion_45 where dni_usuario=$id;";
+// ejecuta la consulta
+$r = $conexion -> query($sql);
+//recorre el recordset
+$salida=mysqli_affected_rows($conexion);
+//cerrado de la conexion
+mysqli_close($conexion);
+//retorna la operacion
+return $salida;
+
